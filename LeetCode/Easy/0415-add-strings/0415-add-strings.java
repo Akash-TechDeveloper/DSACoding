798150da3a0c7085924546df158e4371bbd89062
Akash Akz <akashdharun2020@gmail.com>
2025-08-30 13:47:30 +0530

    Time: 1 ms (100%), Space: 42.8 MB (48.92%) - LeetHub

--- LeetCode/Easy/0415-add-strings/0415-add-strings.java
+++ LeetCode/Easy/0415-add-strings/0415-add-strings.java
@@ -5,17 +5,22 @@ class Solution {
         }
     }
     public static String addStrings(String num1, String num2) {
-        int i=num1.length()-1, j=num2.length()-1, carry=0;
-        String res = "";
-        while (i>=0 || j>=0) {
-            int d1=(i>=0 ? (int)(num1.charAt(i)-'0') : 0);
-            int d2=(j>=0 ? (int)(num2.charAt(j)-'0') : 0);
-            int curr=d1+d2+carry;
-            carry=(curr>=10 ? 1 : 0);
-            res = Integer.toString(curr%10) + res;
-            i--; j--;
+        StringBuilder result = new StringBuilder();
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+
+            int sum = x + y + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+
+            i--;
+            j--;
         }
-        if (carry==1) res = "1" + res;
-        return res;
+        return result.reverse().toString();
     }
 }
