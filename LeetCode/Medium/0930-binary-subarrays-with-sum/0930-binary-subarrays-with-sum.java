38aec7a5c99fbc87eeb978a39b80c67d3f575894
Akash Akz <akashdharun2020@gmail.com>
2025-09-01 14:00:37 +0530

    Time: 21 ms (41.76%), Space: 49.8 MB (20.69%) - LeetHub

--- LeetCode/Medium/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ LeetCode/Medium/0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,19 +1,19 @@
+import java.util.HashMap;
+
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         int n = nums.length;
-        int [] presum = new int [n+1];
-        for(int i=1;i<=n;i++){
-            presum[i]=presum[i-1]+nums[i-1];
-        }
-         int count = 0;
+        int count = 0;
+        int prefix = 0;
         HashMap<Integer, Integer> freq = new HashMap<>();
-        freq.put(0, 1);
+        freq.put(0, 1); 
 
-        for (int i = 1; i <= n; i++) {
-            int curr = presum[i];
-            count += freq.getOrDefault(curr - goal, 0);
-            freq.put(curr, freq.getOrDefault(curr, 0) + 1);
+        for (int num : nums) {
+            prefix += num; 
+            count += freq.getOrDefault(prefix - goal, 0);
+            freq.put(prefix, freq.getOrDefault(prefix, 0) + 1);
         }
+
         return count;
     }
 }
