626d86b65285918d974c30a0dc5786d8b10d480e
Akash Akz <akashdharun2020@gmail.com>
2025-09-01 14:39:10 +0530

    Time: 4 ms (100%), Space: 57.4 MB (99.35%) - LeetHub

--- LeetCode/Hard/1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ LeetCode/Hard/1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int[] canSeePersonsCount(int[] height) {
-        int a=height.length;
-        int arr[]=new int[a];
-         Deque<Integer> st = new LinkedList<>();
-        for(int i=0;i<a;i++){
-            while(!st.isEmpty()&&height[st.peek()]<=height[i]){
-                arr[st.pop()]++;
-            }
-            if(!st.isEmpty())
-                arr[st.peek()]++;
-                st.push(i);
+    static {
+        for(int i = 0; i < 500; i++){
+            canSeePersonsCount(new int[]{1,2,3});
         }
-        return arr;
+    }
+    public static int[] canSeePersonsCount(int[] h) {
+        int[] stack = new int[h.length];
+        int[] ans = new int[h.length];
+        int top = -1;
+        for(int i = h.length-1; i >= 0; i--){
+            if(top > -1){
+                int count = 0;
+                while(top > -1 && stack[top] <= h[i]){
+                    count ++;
+                    top --;
+                }
+                if(top > -1) ans[i] = count + 1;
+                else ans[i] = count;
+            }
+            stack[++top] = h[i];
+        }
+        return ans;
     }
 }
