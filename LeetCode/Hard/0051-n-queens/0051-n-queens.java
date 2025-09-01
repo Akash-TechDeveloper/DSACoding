513e1f7c37a8590490afd1267f1c0ac1fdf1b804
Akash Akz <akashdharun2020@gmail.com>
2025-09-01 14:49:55 +0530

    Time: 2 ms (85.74%), Space: 44.8 MB (89.3%) - LeetHub

--- LeetCode/Hard/0051-n-queens/0051-n-queens.java
+++ LeetCode/Hard/0051-n-queens/0051-n-queens.java
@@ -1,40 +1,40 @@
 class Solution {
-    List<List<String>> queens = new ArrayList<>();
+    List<List<String>> result = new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char [][] board = new char[n][n];
-        for(char[] bard : board){
-            Arrays.fill(bard,'.');
+        char [][] placed = new char[n][n];
+        for(char val[] : placed){
+            Arrays.fill(val,'.');
         }
-        arrange (board,0);
-        return queens;
+        queens(placed,0);
+        return result;
     }
-    public void arrange(char [][] board,int row){
-        if(row==board.length) {
-            List<String> qin = new ArrayList<>();
-            for(int i=0;i<board.length;i++){
-                qin.add(new String(board[i]));
+    public void queens (char [][] place, int row){
+        if(row==place.length){
+            List<String> list = new ArrayList<>();
+            for(int i=0;i<place.length;i++){
+                list.add(new String (place[i]));
             }
-            queens.add(qin);
+            result.add(list);
             return;
         }
-        for(int col=0;col<board[row].length;col++){
-            if(isSafe(board,row,col)) {
-                board[row][col]='Q';
-                arrange(board,row+1);
-                board[row][col]='.';
+        for(int col=0;col<place[row].length;col++){
+            if (isSafe(place , row , col)){
+                place[row][col] = 'Q';
+                queens(place,row+1);
+                place[row][col] = '.';
             }
-        }    
+        }
     }
-    public boolean isSafe(char [][] board ,int row , int col){
+    public boolean isSafe(char [][] board , int row , int col){
         for(int i=0;i<row;i++){
             if(board[i][col]=='Q') return false;
         }
-        int maxLeft = Math.min(row,col);
-        for(int i=1;i<=maxLeft;i++){
+        int maxleft = Math.min(row,col);
+        for(int i=1;i<=maxleft;i++){
             if(board[row-i][col-i]=='Q') return false;
         }
-        int maxRight = Math.min(row,board.length-1-col);
-        for(int i=1;i<=maxRight;i++){
+        int maxright = Math.min(row,board.length-1-col);
+        for(int i=1;i<=maxright;i++){
             if(board[row-i][col+i]=='Q') return false;
         }
         return true;
