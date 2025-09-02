b77bd0ba1ab8f7c6799360ae9191ebab45b1e5af
Akash Akz <akashdharun2020@gmail.com>
2025-09-02 14:48:00 +0530

    Time: 16 ms (99.36%), Space: 54.6 MB (83.42%) - LeetHub

--- LeetCode/Medium/0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
+++ LeetCode/Medium/0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -1,32 +1,30 @@
 class Solution {
-    boolean possible(int len, int[] nums1, int[] nums2) {
-        for (int i = 0; i <= nums1.length - len; i++) {
-            for (int j = 0; j <= nums2.length - len; j++) {
-                boolean match = true;
-                for (int k = 0; k < len; k++) {
-                    if (nums1[i + k] != nums2[j + k]) {
-                        match = false;
-                        break;
-                    }
-                }
-                if (match) {
-                    return true;
-                }
-            }
+    static{
+        int arr[]={0};
+        for(int i=0;i<300;i++){
+            findLength(arr,arr);
         }
-        return false;
     }
-    public int findLength(int[] nums1, int[] nums2) {
-        int low = 1, high = Math.min(nums1.length, nums2.length), ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (possible(mid,nums1, nums2)) {
-                ans = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+    public static int findLength(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int m=nums2.length;
+        int dp[][]=new int[n+1][m+1];
+
+        int max=0;
+        for(int i=1;i<n+1;i++){
+
+            for(int j=1;j<m+1;j++){
+
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+
+                    max=Math.max(dp[i][j],max);
+                }
+                else{
+                    dp[i][j]=0;
+                }
             }
-        }
-        return ans;
+        }        
+        return max;
     }
 }
