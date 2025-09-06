c92654eaf4fdfc7188429b848134f44428870d0b
Akash Akz <akashdharun2020@gmail.com>
2025-09-06 15:06:58 +0530

    Time: 2 ms (72.23%), Space: 61.6 MB (64.66%) - LeetHub

---
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,23 @@
+
+    class Solution {
+        static {
+            int arr[]={0};
+            for(int i=0;i<300;i++){
+                maxProfit(arr);
+            }
+        }
+    public static int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+
+        return profit;        
+    }
+}
