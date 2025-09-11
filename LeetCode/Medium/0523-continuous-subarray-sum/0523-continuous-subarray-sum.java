59bac0fc59b9570f66e01843bdbfeb30f6a300ca
Akash Akz <akashdharun2020@gmail.com>
2025-09-11 10:22:29 +0530

    Time: 23 ms (92.08%), Space: 57.5 MB (77.35%) - LeetHub

---
+++ LeetCode/Medium/0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int presum = 0;
+    //  int presum [] = new int[n+1];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,-1);
+        for(int i=0;i<n;i++){
+    //      presum[i]=presum[i-1]+nums[i-1];
+    //      presum[i]=presum[i]%k;
+            presum+=nums[i];
+            int remain = presum%k;
+            if(remain<0) remain+=k;
+    //        System.out.print(presum[i]);
+            if(map.containsKey(remain)){
+                int index = map.get(remain);
+                if(i-index>=2) return true;
+            }
+            else map.put(remain,i);
+        }
+        return false;
+    }
+}
