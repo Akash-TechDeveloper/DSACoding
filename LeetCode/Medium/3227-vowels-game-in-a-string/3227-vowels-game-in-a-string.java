3e01c8306727a2ca79fa9f61a075f6c3886ec29b
Akash Akz <akashdharun2020@gmail.com>
2025-09-12 09:54:28 +0530

    Time: 2 ms (100%), Space: 45.9 MB (8.66%) - LeetHub

--- LeetCode/Medium/3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
+++ LeetCode/Medium/3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.java
@@ -1,9 +1,16 @@
 class Solution {
-    public boolean doesAliceWin(String s) {
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') return true;
+    static{
+        for(int i=0;i<200;i++) doesAliceWin("a");
+    }
+    public static boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case 'a','e','i','o','u':
+                    return true;
+            }
         }
+
         return false;
     }
 }
