81a1425c9d055051c5c631950f11d95a068e6de4
Akash Akz <akashdharun2020@gmail.com>
2025-09-21 10:49:26 +0530

    Time: 2 ms (89.55%), Space: 42.2 MB (64.35%) - LeetHub

---
+++ LeetCode/Easy/1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int canBeTypedWords(String text, String broken) {
+        Set<Character> brokenSet = new HashSet<>();
+        for (char c : broken.toCharArray()) {
+            brokenSet.add(c);
+        }
+
+        int count = 0;
+        String[] words = text.split(" ");
+        for (String word : words) {
+            boolean canType = true;
+            for (char c : word.toCharArray()) {
+                if (brokenSet.contains(c)) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
