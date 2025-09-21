5b00dda2e55fadfc5dc65da745021a92019613ce
Akash Akz <akashdharun2020@gmail.com>
2025-09-21 16:55:01 +0530

    Time: 13 ms (93.8%), Space: 55.9 MB (27.23%) - LeetHub

--- LeetCode/Medium/1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ LeetCode/Medium/1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,11 +1,10 @@
 class Solution {
     public int minSwaps(String s) {
-        int open = 0, close = 0;
+        int open = 0;
         for(char ch : s.toCharArray()){
             if(ch == '[') open++;
             else if(open > 0) open--;
-            else close++;
         }
-        return (close + 1) / 2;
+        return (open + 1) / 2;
     }
 }
