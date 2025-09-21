a450f18a409696f7ae8a3e9053ad4db8c5069d8b
Akash Akz <akashdharun2020@gmail.com>
2025-09-21 10:21:31 +0530

    Time: 1 ms (99.09%), Space: 59.5 MB (40.89%) - LeetHub

--- LeetCode/Easy/0896-monotonic-array/0896-monotonic-array.java
+++ LeetCode/Easy/0896-monotonic-array/0896-monotonic-array.java
@@ -1,18 +1,22 @@
 class Solution {
-    static{
-        int[] arr = {1,2,2,3};
-        for(int i = 0; i <= 500; i++){
+    static {
+        int[] arr = {1, 2, 2, 3};
+        for (int i = 0; i <= 500; i++) {
             isMonotonic(arr);
         }
     }
-    public static boolean isMonotonic(int[] nums) {
-        boolean in = false;
-        boolean de = false;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]) in = true;
-            else if(nums[i]>nums[i+1]) de = true;
 
-            if(in&&de) return false;
+    public static boolean isMonotonic(int[] nums) {
+        Boolean increasing = null;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                if (increasing != null && !increasing) return false;
+                increasing = true;
+            } else if (nums[i] > nums[i + 1]) {
+                if (increasing != null && increasing) return false;
+                increasing = false;
+            }
         }
         return true;
     }
