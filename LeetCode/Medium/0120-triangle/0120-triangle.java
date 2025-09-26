e718d9b2ed44b5d27272b66381614cedbb8579ba
Akash Akz <akashdharun2020@gmail.com>
2025-09-26 10:22:33 +0530

    Time: 3 ms (78.98%), Space: 45.1 MB (93.26%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,16 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle.isEmpty()||triangle == null)  return 0;
-        
-        for(int i=triangle.size()-2;i>=0;i--){
-            for(int j=0;j<triangle.get(i).size();j++){
-                int curr =triangle.get(i).get(j);
-                int left  = triangle.get(i+1).get(j);
-                int right = triangle.get(i+1).get(j+1);
-                int min = Math.min(left,right);
-                triangle.get(i).set(j,curr+min);
+
+        int rows = triangle.size()-1;
+
+        int[] sum =new int[triangle.get(rows).size()+1];
+
+        for(int i = rows;i>=0;i--){
+            for(int j = 0;j<=i;j++){
+                sum[j] = Math.min(sum[j],sum[j+1])+triangle.get(i).get(j); 
             }
         }
-        return triangle.get(0).get(0);
+
+
+        return sum[0];
+
     }
 }
