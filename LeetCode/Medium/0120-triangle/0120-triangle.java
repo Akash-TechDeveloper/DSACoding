2ba7e536687eae7d8dfb666036e752187b0946d7
Akash Akz <akashdharun2020@gmail.com>
2025-09-26 10:30:08 +0530

    Time: 1 ms (99.85%), Space: 45.5 MB (54.04%) - LeetHub

--- LeetCode/Medium/0120-triangle/0120-triangle.java
+++ LeetCode/Medium/0120-triangle/0120-triangle.java
@@ -1,36 +1,24 @@
 class Solution {
+    Integer dp[][];
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        
-        // dp array initialized with -1
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        
-        return solve(0, 0, triangle, dp);
+        dp = new Integer[n][n];
+        return solve(0, 0, triangle);
     }
     
-    private int solve(int i, int j, List<List<Integer>> triangle, int[][] dp) {
+    public int solve(int i, int j, List<List<Integer>> triangle) {
         int n = triangle.size();
-        
-        // Base case: last row → return value at that cell
         if (i == n - 1) {
             return triangle.get(i).get(j);
         }
         
-        // If already computed, return stored value
-        if (dp[i][j] != -1) {
+        if (dp[i][j] != null) {
             return dp[i][j];
         }
         
-        // Recursive calls: go down and diagonal
-        int down = solve(i + 1, j, triangle, dp);
-        int diagonal = solve(i + 1, j + 1, triangle, dp);
+        int down = solve(i + 1, j, triangle);
+        int diagonal = solve(i + 1, j + 1, triangle);
         
-        // Store result in dp
         dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
         
         return dp[i][j];
