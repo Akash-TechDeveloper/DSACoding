943f3f5f7c01f74c07770973af92ae11e029ca40
Akash Akz <akashdharun2020@gmail.com>
2025-10-04 09:29:21 +0530

    Time: 1 ms (99.65%), Space: 57 MB (96.54%) - LeetHub

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    static {
+        for (int i = 0; i < 500; i++) {
+            maxArea(new int[] { 0, 0 });
+        }
+    }
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;        
+    }
+}
