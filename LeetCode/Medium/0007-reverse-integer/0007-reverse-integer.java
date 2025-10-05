030e1d3c00872981ecae73fed9e9eead932f1e9e
Akash Akz <akashdharun2020@gmail.com>
2025-10-05 11:47:38 +0530

    Time: 1 ms (81.26%), Space: 40.6 MB (94.71%) - LeetHub

--- LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.java
@@ -1,12 +1,13 @@
 class Solution {
     public int reverse(int x) {
         //if(x<0) {x= -1*x;
-        long rev = 0;
-        while(x!=0){
-            rev = (x%10)+ rev*10;
+        int rev = 0;
+        while(x!=0){    
+            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10) return 0;
+            rev = (x%10)+ rev*10;   
             x/=10;
         }
-        if(rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE) return 0;
-        return (int) rev;
+       
+        return rev;
     }
 }
