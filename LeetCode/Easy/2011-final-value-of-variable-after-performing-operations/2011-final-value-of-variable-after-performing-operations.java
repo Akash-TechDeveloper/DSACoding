ee3e98014fcc8fd0ed96959d6415c63bf022b64b
Akash Akz <akashdharun2020@gmail.com>
2025-10-26 09:43:52 +0530

    Time: 0 ms (100%), Space: 43.1 MB (21.39%) - LeetHub

---
+++ LeetCode/Easy/2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int finalValueAfterOperations(String[] op) {
+        int x=0;
+        for(String s : op){
+            if(s.charAt(1)=='+') x++;
+            else x--;
+        }
+        return x;
+    }
+}
