aede0c08f40768c27699564903231272d623733f
Akash Akz <akashdharun2020@gmail.com>
2025-10-29 10:07:20 +0530

    Time: 1 ms (33.25%), Space: 40.9 MB (36.78%) - LeetHub

--- LeetCode/Easy/3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
+++ LeetCode/Easy/3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.java
@@ -1,18 +1,12 @@
 class Solution {
     public int smallestNumber(int n) {
-        
         for(int i=n;i<n*n;i++){
             if(isPower(i)&& ((i-1)>=n)) return i-1;
         }
         return 1;
     }
+
     private static boolean isPower(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        while (n % 2 == 0) {
-            n /= 2;
-        }
-        return n == 1;
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
