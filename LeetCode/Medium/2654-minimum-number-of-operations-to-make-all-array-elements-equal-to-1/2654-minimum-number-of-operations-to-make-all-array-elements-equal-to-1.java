f6668e9d855c45dff5e17327b42244dd9bd1b86c
Akash Akz <akashdharun2020@gmail.com>
2025-11-12 12:01:09 +0530

    Time: 1 ms (100%), Space: 43.6 MB (13.7%) - LeetHub

--- LeetCode/Medium/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
+++ LeetCode/Medium/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2654-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.java
@@ -2,11 +2,13 @@ class Solution {
     public int minOperations(int[] nums) {
         int n= nums.length;
         int ones =0;
+        int x= 0;
         for(int num : nums){
             if(num==1) ones++;
+            x = gcd(x,num);
         }
-        if(ones>0) return n - ones;
-
+        if (ones > 0) return n - ones;
+        if (x > 1) return -1;
         int ans = Integer.MAX_VALUE;
         for(int i=0;i<n-1;i++){
             int g = nums[i];
