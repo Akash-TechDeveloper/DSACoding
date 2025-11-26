7542d40440b379264b56f518d36fc559d911a678
Akash Akz <akashdharun2020@gmail.com>
2025-11-26 10:31:20 +0530

    Time: 120 ms (12.16%), Space: 96.7 MB (27.02%) - LeetHub

--- 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
+++ 2435-paths-in-matrix-whose-sum-is-divisible-by-k/2435-paths-in-matrix-whose-sum-is-divisible-by-k.java
@@ -1,25 +1,43 @@
 class Solution {
-    int n, m;
-    static final int MOD = 1_000_000_007;
+    private int n, m, k;
+    private static final int MOD = 1_000_000_007;
 
     public int numberOfPaths(int[][] grid, int k) {
-        n = grid.length;  m = grid[0].length;
-        int[][][] dp = new int[n][m][k];
-        for (int[][] row : dp) for (int[] mod : row) Arrays.fill(mod, -1);
+        this.n = grid.length;
+        this.m = grid[0].length;
+        this.k = k;
 
-        return paths(dp, grid, 0, 0, grid[0][0] % k, k);
+        int[][][] dp = new int[n][m][k];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        return dfs(grid, dp, 0, 0, grid[0][0] % k);
     }
 
-    private int paths(int[][][] dp, int[][] grid, int r, int c, int mod, int k) {
+    private int dfs(int[][] grid, int[][][] dp, int r, int c, int mod) {
 
         if (r == n - 1 && c == m - 1) return mod == 0 ? 1 : 0;
-        if (dp[r][c][mod] != -1) return dp[r][c][mod];
-
+        int memo = dp[r][c][mod];
+        if (memo != -1) return memo;
         long res = 0;
 
-        if (r +1< n) res = (res + paths(dp, grid, r + 1, c, (mod + grid[r + 1][c]) % k, k)) % MOD;
-        if (c + 1 < m) res = (res + paths(dp, grid, r, c + 1, (mod + grid[r][c + 1]) % k, k)) % MOD;
+        if (r + 1 < n) {
+            int nextMod = mod + grid[r + 1][c];
+            if (nextMod >= k) nextMod %= k;
+            res += dfs(grid, dp, r + 1, c, nextMod);
+            if (res >= MOD) res %= MOD;
+        }
 
+        if (c + 1 < m) {
+            int nextMod = mod + grid[r][c + 1];
+            if (nextMod >= k) nextMod %= k;
+            res += dfs(grid, dp, r, c + 1, nextMod);
+            if (res >= MOD) res %= MOD;
+        }
         return dp[r][c][mod] = (int) res;
     }
 }
